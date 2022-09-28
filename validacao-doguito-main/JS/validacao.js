7438be5d78e8559b6d8d4cdb1f6444629273651f
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-09-27 22:11:56 -0300

    Criando arquivo de validação

--- validacao-doguito-main/JS/validacao.js
+++ validacao-doguito-main/JS/validacao.js
@@ -1,4 +1,14 @@
-const dataNascimento = document.querySelector('#nascimento')
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+}
+
+const validadores = {
+    dataNascimento: input => validaDataNascimento(input)
+}
 
 dataNascimento.addEventListener('blur', (evento) => {
     validaDataNascimento(evento.target)    
